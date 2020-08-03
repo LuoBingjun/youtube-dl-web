ad3b594f015af1b8f83d973049d890e487b55893
Bingjun Luo <luobingjun@gmail.com>
2020-08-03 23:06:05 +0800

    add URL_PATH

--- src/flask-back/app.py
+++ src/flask-back/app.py
@@ -3,10 +3,12 @@ from flask_restful import Api
 
 from video_info import VideoInfo
 
+URL_PATH = '/ytb'
+
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(VideoInfo, '/api/video_info')
+api.add_resource(VideoInfo, URL_PATH + '/api/video_info')
 
 if __name__ == '__main__':
     app.run(debug=True)
