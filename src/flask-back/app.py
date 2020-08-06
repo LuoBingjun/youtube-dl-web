19258416e44951bf350cec205984f88a4bcffb86
Bingjun Luo <luobingjun@gmail.com>
2020-08-06 21:01:49 +0800

    add static support

--- src/flask-back/app.py
+++ src/flask-back/app.py
@@ -5,14 +5,22 @@ from video_info import VideoInfo
 from download import Download
 import config
 
-
-app = Flask(__name__,)
+app = Flask(
+    __name__,
+    static_url_path=config.URL_PATH
+)
 
 app.config['JSON_AS_ASCII'] = False
 
 api = Api(app)
 api.add_resource(VideoInfo, config.URL_PATH + '/api/video_info')
 api.add_resource(Download, config.URL_PATH + '/api/download')
+
+
+@app.route(config.URL_PATH + '/')
+def index():
+    return app.send_static_file('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
